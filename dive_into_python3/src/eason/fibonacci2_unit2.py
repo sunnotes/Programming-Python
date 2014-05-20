ddf4fdae099c1ba67e14db9f7908531f57e2e43b
eason <liliangjun88@gmail.com>
2014-05-20 17:12:06 +0800

    新增

---
+++ dive_into_python3/src/eason/fibonacci2_unit2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf8 -*-
+'''
+Created on 2014年5月20日
+
+@author: EASON
+'''
+import fibonacci2
+
+if __name__ == '__main__':
+    fib = fibonacci2.Fib(100)
+    print(fib)
+    print(fib.max)
